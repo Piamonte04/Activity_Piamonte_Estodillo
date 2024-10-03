a3b70bf0541a0ed03037aae6bd3fc4fce87ef90b
Lenovo <Lenovo@EEE-G8G64MN>
2024-10-03 22:07:35 +0800

    final mean

--- Activity_Piamonte_Estodillo/src/activity_piamonte_estodillo/Activity_Piamonte_Estodillo.java
+++ Activity_Piamonte_Estodillo/src/activity_piamonte_estodillo/Activity_Piamonte_Estodillo.java
@@ -12,60 +12,77 @@ import java.util.Scanner;
  */
 public class Activity_Piamonte_Estodillo {
      
-    static double total, difference, quotient, product;
-    static double num1, num2, num3;
+
+    static double numbers [] = new double[10], x, mean,
+                       sum=0, sum2=0, counter=0;
    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World!!!!!!");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter 10 munbers, one at a time: ");
         
-        Scanner sc = new Scanner(System.in);
-        
-        boolean loop = true;
-        
-        while (true){
-        
-        int operation;
-        System.out.print("Num 1: ");
-        num1 = sc.nextDouble();
-        System.out.print("Num 2: ");
-        num2 = sc.nextDouble();
-        System.out.print("Num 3: ");
-        num3 = sc.nextDouble();
-        System.out.println("Choose an operation: 1. addition 2.subtraction 3.division 4.multiplication");
-        operation = sc.nextInt();
-        switch (operation){
-            case 1:
-                total = num1 + num2 + num3;
-                System.out.println("Total: " + total);
-                break;        
-            case 2:
-                difference = num1 - num2 - num3;
-                System.out.println("Difference: " + difference);
-                break;
-            case 3:
-                quotient = num1 / num2 / num3;
-                System.out.println("Quotient: " + quotient);
-                break;
-            case 4:
-                product = num1 * num2 * num3;
-                System.out.println("{Product: " + product);
-                break;
-            }
-            System.out.println("Do you want to calculate another numbers?");
-            sc.nextLine();
-            System.out.println("Type: y/n ");
-            String calculate = sc. nextLine();
-            
-            if (calculate == "y"){
-                loop= true;
-            }
-            else{
-                loop = false;
-            }
+        for (int i = 0;i<numbers.length;i++) {
+        x= scn.nextDouble();
+          numbers[i]= x;
+          sum+=numbers[i];
         }
+        mean = sum / numbers.length;
+        for (int i = 0;i<numbers.length;i++){
+            sum2 += Math.pow(numbers[i] - mean, 2);
+        }
+        System.out.println("Mean = " + mean );
         
         
-    }
+        
+        
+//       System.out.ptln("Hello World!!!!!!");
+//        
+//        Scanner sc = new Scanner(System.in);
+//        
+//        boolean loop = true;
+//        
+//        while (true){
+//        
+//        int operation;
+//        System.out.print("Num 1: ");
+//        num1 = sc.nextDouble();
+//        System.out.print("Num 2: ");
+//        num2 = sc.nextDouble();
+//        System.out.print("Num 3: ");
+//        num3 = sc.nextDouble();
+//        System.out.println("Choose an operation: 1. addition 2.subtraction 3.division 4.multiplication");
+//        operation = sc.nextInt();
+//        switch (operation){
+//            case 1:
+//                total = num1 + num2 + num3;
+//                System.out.println("Total: " + total);
+//                break;        
+//            case 2:
+//                difference = num1 - num2 - num3;
+//                System.out.println("Difference: " + difference);
+//                break;
+//            case 3:
+//                quotient = num1 / num2 / num3;
+//                System.out.println("Quotient: " + quotient);
+//                break;
+//            case 4:
+//                product = num1 * num2 * num3;
+//                Sysem.out.println("{Product: " + product);
+//                break;
+//            }
+//            System.out.println("Do you want to calculate another numbers?");
+//            sc.nextLine();
+//            System.out.println("Type: y/n ");
+//            String calculate = sc. nextLine();
+//            
+//            if (calculate == "y"){
+//                loop= true;
+//            }
+//            else{
+//                loop = false;
+//            }
+//        }
+//        
+//        
+//    }
     
-}
+}}
